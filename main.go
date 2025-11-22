cca06abff87d4b692dc2a499b4fa532ce302e644
bsa <johndoe@example.com>
2025-11-23 02:34:38 +0300

    add worker pool

--- main.go
+++ main.go
@@ -1,5 +1,71 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
 
+func main() {
+	start := time.Now()
+	workerPool()
+	elapsed := time.Since(start)
+	fmt.Println("Elapsed time: ", elapsed)
+}
+
+func workerPool() {
+	ctx, cancel := context.WithCancel(context.Background())
+	//ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*20)
+	defer cancel()
+
+	wg := sync.WaitGroup{}
+	numbersToProcess, processedNumbers := make(chan int, 5), make(chan int, 5)
+
+	for i := 0; i <= runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			worker(ctx, numbersToProcess, processedNumbers)
+		}()
+	}
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			/*if i == 500 {
+			  cancel()
+			*/
+			numbersToProcess <- i
+		}
+		close(numbersToProcess)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(processedNumbers)
+	}()
+
+	var counter int
+	for resultValue := range processedNumbers {
+		counter++
+		fmt.Printf("Результат %d: %d\n", counter, resultValue)
+	}
+
+	fmt.Println("Значений обработано:", counter)
+}
+
+func worker(ctx context.Context, toProcess <-chan int, processed chan<- int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case value, ok := <-toProcess:
+			if !ok {
+				return
+			}
+			time.Sleep(time.Millisecond)
+			processed <- value * value
+		}
+	}
 }
